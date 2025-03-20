e729abe00eae01006641c062fb846337ae19f876
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2025-03-20 18:35:35 -0500

    Comment out service routes and job dispatches in API routes

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,16 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/servicio-1', function (Request $request) {
+// Route::get('/servicio-1', function (Request $request) {
 
-    PythonServiceJob::dispatch('type1', 'https://mi-url.com/image.jpg');
-    return response()->json(['message' => 'servicio 1 solicitado']);
-    // return $request->user();
-});
+//     PythonServiceJob::dispatch('type1', 'https://mi-url.com/image.jpg');
+//     return response()->json(['message' => 'servicio 1 solicitado']);
+//     // return $request->user();
+// });
 
-Route::get('/servicio-2', function (Request $request) {
+// Route::get('/servicio-2', function (Request $request) {
 
-    PythonServiceJob::dispatch('type2', 'https://mi-url.com/image.jpg');
-    return response()->json(['message' => 'servicio 2 solicitado']);
-    // return $request->user();
-});
+//     PythonServiceJob::dispatch('type2', 'https://mi-url.com/image.jpg');
+//     return response()->json(['message' => 'servicio 2 solicitado']);
+//     // return $request->user();
+// });
